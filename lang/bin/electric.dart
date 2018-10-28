759ef24b37399945679542a1ff51388e47312da7
Matthew Blount <xkapastel@gmail.com>
2018-10-28 08:39:40 -0400

    Initial.

---
+++ lang/bin/electric.dart
@@ -0,0 +1,6 @@
+import "package:electric/electric.dart" as electric;
+
+main() {
+  var value = electric.Value();
+  value.debug();
+}
---
+++ lang/lib/electric.dart
@@ -0,0 +1,5 @@
+class Value {
+  debug() {
+    print("electric parens");
+  }
+}
